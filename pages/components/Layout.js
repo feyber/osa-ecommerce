d5d0f8b89ae0f29a308acba8295e5eb1c84de9cb
feyber <feyberho@gmail.com>
2021-09-28 02:42:31 +0700

    connect to mongodb

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -7,18 +7,19 @@ import {
   createTheme,
   ThemeProvider,
   CssBaseline,
-  Switch,
+  // Switch,
 } from '@material-ui/core'
 import Head from 'next/head'
 import Image from 'next/image'
 import useStyle from '../../utils/styles'
 import NextLink from 'next/link'
-import { useContext } from 'react'
-import { Store } from '../../utils/Store'
+// import { useContext } from 'react'
+// import { Store } from '../../utils/Store'
+// import Cookies from 'js-cookie'
 
 export default function LayoutPage({ title, children, description }) {
-  const { state, dispatch } = useContext(Store)
-  const { darkMode } = state
+  // const { state, dispatch } = useContext(Store)
+  // const { darkMode } = state
   const theme = createTheme({
     typography: {
       h1: {
@@ -33,7 +34,8 @@ export default function LayoutPage({ title, children, description }) {
       },
     },
     pallete: {
-      type: darkMode ? 'dark' : 'light',
+      // type: darkMode ? 'dark' : 'light',
+      type: 'light',
       primary: {
         main: '#f0c000',
       },
@@ -43,9 +45,11 @@ export default function LayoutPage({ title, children, description }) {
     },
   })
   const classes = useStyle()
-  const darkModeChangeHandler = () => {
-    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' })
-  }
+  // const darkModeChangeHandler = () => {
+  //   dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' })
+  //   const newDarkMode = !darkMode
+  //   Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF')
+  // }
   return (
     <div>
       <Head>
@@ -68,10 +72,10 @@ export default function LayoutPage({ title, children, description }) {
             </NextLink>
             <div className={classes.kanan}></div>
             <div>
-              <Switch
+              {/* <Switch
                 checked={darkMode}
                 onChange={darkModeChangeHandler}
-              ></Switch>
+              ></Switch> */}
               <NextLink href='/cart' passHref>
                 <Link className={classes.bold}>Cart</Link>
               </NextLink>
